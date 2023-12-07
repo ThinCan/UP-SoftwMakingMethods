ebca361457f1d78b7aaa4ff6fd67aaf77fb39011
ThinCan <01169581@pw.edu.pl>
2023-12-07 19:52:17 +0100

    p

--- index.py
+++ index.py
@@ -16,12 +16,12 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get("http://83.5.245.147:5500/")
 driver.implicitly_wait(0.1)
 book_row = driver.find_element(By.CLASS_NAME, "book-row")
-span1 = book_row.find_element(By.CSS_SELECTOR, "span:nth-child(1)")
-span2 = book_row.find_element(By.CSS_SELECTOR, "span:nth-child(2)")
-span3 = book_row.find_element(By.CSS_SELECTOR, "span:nth-child(3)")
+span1 = driver.find_element(By.CSS_SELECTOR, ".book-row > span:nth-child(1)")
+span2 = driver.find_element(By.CSS_SELECTOR, ".book-row > span:nth-child(2)")
+span3 = driver.find_element(By.CSS_SELECTOR, ".book-row > span:nth-child(3)")
 if span1 == None or span2 == None or span3 == None:
     raise RuntimeError("UI isn't loaded correctly.")
-if span1.text != 'a' or span2.text != 'b' or span3 != 'c':
+if span1.text != 'a' or span2.text != 'b' or span3.text != 'c':
     raise RuntimeError("UI doesn't contain expected text.")
 
     # if span1 == "a" and span2 == "b" and span3 == "c":
