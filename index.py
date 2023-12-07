6aaf4fe6f2f14f3a107fa671bd9ee4ab166c3e46
ThinCan <01169581@pw.edu.pl>
2023-12-07 19:48:33 +0100

    as

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 options = Options()
 options.add_argument('--headless')
@@ -10,6 +12,21 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://python.org")
-print(driver.title)
-driver.close()
+
+driver.get("http://83.5.245.147:5500/")
+driver.implicitly_wait(0.1)
+book_row = driver.find_element(By.CLASS_NAME, "book-row")
+span1 = book_row.find_element(By.CSS_SELECTOR, "span:nth-child(1)")
+span2 = book_row.find_element(By.CSS_SELECTOR, "span:nth-child(2)")
+span3 = book_row.find_element(By.CSS_SELECTOR, "span:nth-child(3)")
+if span1 == None or span2 == None or span3 == None:
+    raise RuntimeError("UI isn't loaded correctly.")
+if span1.text != 'a' or span2.text != 'b' or span3 != 'c':
+    raise RuntimeError("UI doesn't contain expected text.")
+
+    # if span1 == "a" and span2 == "b" and span3 == "c":
+    #     print("The book-row element has three spans next to each other with values: a, b, c")
+    # else:
+    #     raise Exception("The book-row element does not have three spans next to each other with values: a, b, c")
+
+driver.quit()
